86156dd00e01fd1468d8b7f08c51ee2c9408c84d
Olivier Fournier, B2B-CDL-CSO-CMS <Olivier.Fournier@swisscom.com>
2020-12-17 16:39:42 +0100

    Test auth

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,7 +1,7 @@
-variable "organization" {
-  description = "GitHub organization used to configure the provider"
-  type        = string
-}
+#variable "organization" {
+#  description = "GitHub organization used to configure the provider"
+#  type        = string
+#}
 
 #variable "github_token" {
 #  description = "GitHub access token used to configure the provider"
