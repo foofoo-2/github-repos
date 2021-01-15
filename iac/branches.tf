384bc5f0a6b5cf481b467198fe677b54a940273e
Olivier Fournier, B2B-CDL-CSS-CMS <Olivier.Fournier@swisscom.com>
2021-01-15 11:04:58 +0100

    Add master branch protection

--- iac/branches.tf
+++ iac/branches.tf
@@ -2,7 +2,7 @@ resource "github_branch_protection" "branch_protection" {
   for_each       = var.repositories
 
   repository_id  = github_repository.repository[each.key].node_id
-  pattern        = "main"
+  pattern        = "[main,master]*"
 
   enforce_admins = true
 
