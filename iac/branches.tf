e0c36da29aca6e108bb1e59b34f74a252280ae61
Olivier Fournier, B2B-CDL-CSS-CMS <Olivier.Fournier@swisscom.com>
2021-01-15 11:00:05 +0100

    Fix branch protection

--- iac/branches.tf
+++ iac/branches.tf
@@ -1,7 +1,7 @@
 resource "github_branch_protection" "branch_protection" {
   for_each       = var.repositories
 
-  repository_id  = github_repository.repository.node_id
+  repository_id  = github_repository.repository[each.key].node_id
   pattern        = "main"
 
   enforce_admins = true
